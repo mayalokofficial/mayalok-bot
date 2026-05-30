455ab44c2312a10bf253bb8670ff329d116e1912
mayalokofficial <mdhafizurrahman4578@gmail.com>
2026-05-30 12:15:51 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQu
 
 # Config
 TOKEN = os.environ.get("TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 0)) or None
+github.com/mayalokofficial/mayalok-bot/blob/main/bot.py
 users = {}
 videos = {}
 scripts = {}
